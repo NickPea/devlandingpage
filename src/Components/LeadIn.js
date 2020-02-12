416b8794246474061cfde55d96a02d63c63695cf
Nick <nickp2287@gmail.com>
2020-02-13 01:12:26 +1000

    made a lead in title and parallax-ified

---
+++ src/Components/LeadIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+export default function LeadIn() {
+  return (
+    <div className="d-flex justify-content-center">
+      <Card body>
+        <h1>Hi Im Nick</h1>
+      </Card>
+    </div>
+  );
+}
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,11 +2,14 @@ import React from "react";
 import Styles from "./LandingPage.module.css";
 import { Paper } from "@material-ui/core";
 import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
+//components
+import LeadIn from "./Components/LeadIn";
 
 export default function LandingPage() {
   return (
     <div>
       <Parallax pages={4}>
+        {/* background images */}
         <ParallaxLayer factor={1} offset={0.5} speed={0}>
           <div className={Styles.bgImage1}></div>
         </ParallaxLayer>
@@ -16,9 +19,9 @@ export default function LandingPage() {
         <ParallaxLayer factor={1} offset={2.5} speed={0}>
           <div className={Styles.bgImage3}></div>
         </ParallaxLayer>
-
+        {/* content panes */}
         <ParallaxLayer factor={0.9} offset={0} speed={0.5}>
-          <Paper className={Styles.stretchMe}>content</Paper>
+          <Paper className={Styles.stretchMe}></Paper>
         </ParallaxLayer>
         <ParallaxLayer factor={0.8} offset={1.1} speed={0.5}>
           <Paper className={Styles.stretchMe}>content</Paper>
@@ -28,6 +31,10 @@ export default function LandingPage() {
         </ParallaxLayer>
         <ParallaxLayer factor={0.9} offset={3.1} speed={0.5}>
           <Paper className={Styles.stretchMe}>content</Paper>
+        </ParallaxLayer>
+        {/* quicker moving bits and pieces */}
+        <ParallaxLayer factor={1} offset={0.3} speed={2}>
+          <LeadIn />
         </ParallaxLayer>
       </Parallax>
     </div>
